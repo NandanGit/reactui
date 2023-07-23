2df3d54eb1be0cb8d3d6056cbd54bb19e689ae0d
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-07-23 21:52:57 +0530

    Update Button

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../components';
 import React, { useState } from 'react';
-import { useInteractionState } from '../hooks/useInteractionState';
+import background from "./assets/background.svg"
 
 
 //👇 This default export determines where your story goes in the story list
@@ -25,31 +25,34 @@ type Story = StoryObj<typeof Button>;
 // Custom Component
 const Btn: typeof Button = ({className,...props}) => {
   return <Button 
-  className={`${className} border-none glass rounded-md w-fit hover:cursor-pointer hover:scale-[1.02] outline-none transition-all`} 
+  className={`${className} border-none glass rounded-md hover:cursor-pointer hover:scale-[1.02] outline-none transition-all`} 
   classNameBySize={{
     sm: 'text-[0.75rem] px-[0.6rem] py-[0.3rem]',
     md: 'text-[1rem] px-3 py-2',
     lg: 'text-[1.25rem] px-4 py-3',
   }}
   classNameByState={{
-    disabled: 'opacity-50 hover:cursor-not-allowed hover:scale-100',
-    pressed: 'opacity-80 transform scale-[0.98]',
+    disabled: 'disabled',
+    pressed: 'transform scale-[0.98]',
     focused: 'ring-1 ring-offset-gray-100 ring-white ring-opacity-60',
   }}
   styleByState={{
     pressed: {
       transform: 'scale(0.98)',
     },
-    disabled: {
-      cursor: "not-allowed"
-    }
   }}
   
   {...props} />
 }
 
 const ButtonsContainer: React.FC<{children: React.ReactNode, className?:string, style?: React.CSSProperties,horizontal?:boolean}> = ({children, className="", style={}, horizontal=false}) => {
-  return <div className={`${className} h-[95vh] w-full flex ${horizontal ? "flex-row space-x-2" : "flex-col space-y-2"} justify-center items-center bg-[#1f1f1f]`} style={style}>
+  return <div className={`${className} h-[95vh] w-full flex ${horizontal ? "flex-row space-x-2" : "flex-col space-y-2"} justify-center items-center bg-[url(./assets/background.svg)]`} style={{
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+
+    ...style,
+  }}>
     {children}
   </div>
 }
@@ -91,12 +94,9 @@ export const InteractionStates: Story = {
       }
     }}
     >{buttonText}</Btn>
-    <Btn isDisabled>Disabled</Btn>
-    {/* {(()=>{
-      const interactionState = useInteractionState()
-      console.log(interactionState)
-      return <button>Hello</button>
-    })()} */}
+    <Btn isDisabled className="" style={{
+      // color: "#fff"
+    }}>Disabled</Btn>
   </ButtonsContainer>
   },
 }
