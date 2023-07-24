fbabe6eccfb97c5a59c368a77178ff5b1a0fbe68
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-07-25 00:13:47 +0530

    Update resolveStyles.ts
    Added JSDoc

--- src/components/utils/resolveStyles.ts
+++ src/components/utils/resolveStyles.ts
@@ -1,5 +1,44 @@
 type PartialRecord<TKey extends string, TValue> = Partial<Record<TKey, TValue>>;
 
+/**
+ * Resolves styles based on a set of keys, classNameMap, styleMap, and conditionsMap.
+ * @param keys - An array of keys to resolve styles for.
+ * @param classNameMap - A map of keys to class names.
+ * @param styleMap - A map of keys to CSS properties.
+ * @param conditionsMap - A map of keys to boolean values.
+ * @returns A map of keys to objects containing className and style.
+ * @example
+ * const buttonVariants = ['filled', 'outline', 'link'];
+ * const classNameMap = {
+ *   filled: 'btn-filled',
+ *   outline: 'btn-outline'
+ * };
+ * const styleMap = {
+ *   filled: { color: 'red' },
+ *   link: { color: 'blue' }
+ * };
+ * const conditionsMap = {
+ *   filled: true,
+ *   outline: false,
+ *   link: true
+ * };
+ * const result = resolveStyles(buttonVariants, classNameMap, styleMap, conditionsMap);
+ *
+ * // result = {
+ * //   filled: {
+ * //     className: 'btn-filled',
+ * //     style: { color: 'red' }
+ * //   },
+ * //   outline: {
+ * //     className: '',
+ * //     style: {}
+ * //   },
+ * //   link: {
+ * //     className: '',
+ * //     style: { color: 'blue' }
+ * //   },
+ * // }
+ * */
 export const resolveStyles = <Key extends string>(
   keys: readonly Key[],
   classNameMap: PartialRecord<Key, string>,
