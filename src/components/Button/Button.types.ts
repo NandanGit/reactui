71d2f15c4eb20b617b2185f53380e4697452b02e
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-07-28 01:09:43 +0530

    Clean
    Removed unused types and some comments

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,38 +1,7 @@
-import {
-  ComponentSize,
-  ComponentAttributeMap,
-  InteractionState,
-  ComponentStatus,
-} from '../types';
+import { ComponentSize, InteractionState, ComponentStatus } from '../types';
 
 export type ButtonSize = Exclude<ComponentSize, 'xs' | 'xl'>;
-export type ButtonSizeToClassNameMap = ComponentAttributeMap<
-  ButtonSize,
-  string
->;
-export type ButtonSizeToStyleMap = ComponentAttributeMap<
-  ButtonSize,
-  React.CSSProperties
->;
+export type ButtonStatus = ComponentStatus | 'primary' | 'secondary';
+export type ButtonVariant = 'filled' | 'outline' | 'ghost' | 'link';
 
 export type ButtonInteractionState = InteractionState;
-
-export type ButtonStatus = ComponentStatus | 'primary' | 'secondary';
-export type ButtonStatusToClassNameMap = ComponentAttributeMap<
-  ButtonStatus,
-  string
->;
-export type ButtonStatusToStyleMap = ComponentAttributeMap<
-  ButtonStatus,
-  React.CSSProperties
->;
-
-export type ButtonVariant = 'filled' | 'outline' | 'ghost' | 'link';
-export type ButtonVariantToClassNameMap = ComponentAttributeMap<
-  ButtonVariant,
-  string
->;
-export type ButtonVariantToStyleMap = ComponentAttributeMap<
-  ButtonVariant,
-  React.CSSProperties
->;
--- src/components/utils/resolveDynamicStyes.ts
+++ src/components/utils/resolveDynamicStyes.ts
@@ -1,48 +1,7 @@
 import { ClassNameMap, StyleMap } from '../../types';
 
-/**
- * Resolves styles based on a set of keys, classNameMap, styleMap, and conditionsMap.
- * @param keys - An array of keys to resolve styles for.
- * @param classNameMap - A map of keys to class names.
- * @param styleMap - A map of keys to CSS properties.
- * @param conditionsMap - A map of keys to boolean values.
- * @returns A map of keys to objects containing className and style.
- * @example
- * const buttonVariants = ['filled', 'outline', 'link'];
- * const classNameMap = {
- *   filled: 'btn-filled',
- *   outline: 'btn-outline'
- * };
- * const styleMap = {
- *   filled: { color: 'red' },
- *   link: { color: 'blue' }
- * };
- * const conditionsMap = {
- *   filled: true,
- *   outline: false,
- *   link: true
- * };
- * const result = resolveStyles(buttonVariants, classNameMap, styleMap, conditionsMap);
- *
- * // result = {
- * //   filled: {
- * //     className: 'btn-filled',
- * //     style: { color: 'red' }
- * //   },
- * //   outline: {
- * //     className: '',
- * //     style: {}
- * //   },
- * //   link: {
- * //     className: '',
- * //     style: { color: 'blue' }
- * //   },
- * // }
- * */
 export const resolveDynamicStyles = <Keys extends readonly string[]>(
   keys: Keys,
-  // classNameMap: ClassNameMap<Keys[number]>,
-  // styleMap: StyleMap<Keys[number]>,
   styles:
     | {
         classNameMap?: ClassNameMap<Keys[number]>;
@@ -55,22 +14,6 @@ export const resolveDynamicStyles = <Keys extends readonly string[]>(
     const isActive = conditionsMap[key as Keys[number]];
     const style = styles?.styleMap?.[key as Keys[number]] || {};
     const className = styles?.classNameMap?.[key as Keys[number]] || '';
-    // if (isActive) {
-    //   return {
-    //     ...acc,
-    //     [key]: {
-    //       style,
-    //       className,
-    //     },
-    //   };
-    // }
-    // return {
-    //   ...acc,
-    //   [key]: {
-    //     style: {},
-    //     className: '',
-    //   },
-    // };
     return {
       ...acc,
       [key]: {
@@ -80,8 +23,3 @@ export const resolveDynamicStyles = <Keys extends readonly string[]>(
     };
   }, {}) as ReturnType<typeof resolveDynamicStyles>;
 };
-
-// type UnionFromArray<TArray extends readonly any[]> = TArray[number];
-
-// const people = ["John", "Mary", "Joe"] as const;
-// type Person = UnionFromArray<typeof people>; // "John" | "Mary" | "Joe"
